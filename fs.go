e8e0af0cc57f9a661da703c9f77a8bd5e37ae4ea
mason <marraison@gmail.com>
2014-01-12 18:41:16 +0100

    fix comment

--- fs.go
+++ fs.go
@@ -3,9 +3,9 @@ Package ramfs implements 9P2000 file server keeping all files in
 memory.
 
 A 9P2000 server is an agent that provides one or more hierarchical
-file systems -- file trees -- that may be accessed by Plan 9
-processes. A server responds to requests by clients to navigate the
-hierarchy, and to create, remove, read, and write files.
+file systems -- file trees -- that may be accessed by processes.
+A server responds to requests by clients to navigate the hierarchy,
+and to create, remove, read, and write files.
 
 References:
   [intro]   http://plan9.bell-labs.com/magic/man2html/5/0intro
